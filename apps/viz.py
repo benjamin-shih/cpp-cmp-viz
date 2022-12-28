b8f6633d2669614aab7be2f0da48f7b6553574a3
Benjamin Shih <benjamin_shih@brown.edu>
2022-12-27 23:33:52 -0800

    python wrapper for rejection sampling complete

--- apps/viz.py
+++ apps/viz.py
@@ -7,14 +7,14 @@ import seaborn as sns
 
 sns.set_theme()
 
-from CPPViz import unif_rect
+from CPPViz import RejectionSample
 
 N = int(1e6)
 
 g = lambda t: np.sin(10 * t) ** 2 * abs(t**3 + 2 * t - 3) if ((t > -1 and t < 0) or (t > 1 / 2 and t < 1)) else 0
-# rej = RejectionSample()
-# rej.rejection_sample(g, N, -1, 1, 0, 10)
-# x = rej.res
+rej = RejectionSample()
+rej.rejection_sample(g, N, -1, 1, 0, 10)
+x = rej.res
 
 sns.histplot(x)
 plt.show()
--- src/pywrap.cpp
+++ src/pywrap.cpp
@@ -1,10 +1,12 @@
 #include <cpp_viz/lib.hpp>
+#include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 constexpr auto byref = py::return_value_policy::reference_internal;
 
-PYBIND11_MODULE(RejectionSample, m) {
+PYBIND11_MODULE(CPPViz, m) {
   py::class_<RejectionSample>(m, "RejectionSample")
       .def(py::init<>())
       .def("rejection_sample", &RejectionSample::rejection_sample,
